358fb1c7516360d5d16973e66e93766b4cace2ef
CTheeG <107345987+C-Thee-G@users.noreply.github.com>
2026-03-15 01:07:22 +0200

    Refactor SmartHome class to manage Room pointers

--- SmartHome.h
+++ SmartHome.h
@@ -1,26 +1,66 @@
 #ifndef SMARTHOME_H
 #define SMARTHOME_H
 
-#include <vector>
 #include "Room.h"
+#include <vector>
 
 class SmartHome {
 private:
-    vector<Room> rooms;
+    vector<Room*> rooms;
+    string homeName;
 
 public:
-
-    void addRoom(Room room) {
-        rooms.push_back(room);
+    SmartHome(string name) {
+        homeName = name;
     }
-
-    void showAllStatus() {
-
-        cout << "\n===== SMART HOME STATUS =====\n";
-
-        for (auto &room : rooms) {
-            room.showStatus();
+    
+    ~SmartHome() {
+        // clean up all rooms
+        for (auto room : rooms) {
+            delete room;
         }
+    }
+    
+    void addRoom(Room* room) {
+        rooms.push_back(room);
+        cout << "Room '" << room->getName() << "' added to " << homeName << endl;
+    }
+    
+    Room* getRoom(int index) {
+        if (index >= 0 && index < rooms.size()) {
+            return rooms[index];
+        }
+        return nullptr;
+    }
+    
+    int getRoomCount() {
+        return rooms.size();
+    }
+    
+    void showAllRooms() {
+        cout << "\n=== " << homeName << " - All Rooms ===" << endl;
+        if (rooms.empty()) {
+            cout << "   No rooms in this smart home" << endl;
+            return;
+        }
+        for (int i = 0; i < rooms.size(); i++) {
+            cout << "   " << (i + 1) << ". " << rooms[i]->getName() << endl;
+        }
+    }
+    
+    void showAllDevices() {
+        cout << "\n=== " << homeName << " - All Devices ===" << endl;
+        if (rooms.empty()) {
+            cout << "   No rooms to display" << endl;
+            return;
+        }
+        for (auto room : rooms) {
+            room->showDevices();
+        }
+    }
+    
+    string getName() {
+        return homeName;
     }
 };
 
